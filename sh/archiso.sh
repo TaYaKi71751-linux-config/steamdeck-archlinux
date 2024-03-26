d2193bbb262b14369af9928beaa416842e444a29
TaYaKi71751 <56256195+TaYaKi71751@users.noreply.github.com>
2024-03-26 10:42:31 +0900

    Update archiso.sh

--- sh/archiso.sh
+++ sh/archiso.sh
@@ -71,7 +71,7 @@ useradd -mG wheel deck
 pacman-key --init
 pacman-key --populate
 pacman -Syyu --noconfirm
-pacman -S sddm plasma-meta kde-{utilities-meta,network-meta} git vim gamescope  --noconfirm
+pacman -S sddm gnome git vim gamescope  --noconfirm
 pacman -S linux-zen linux-zen-headers --noconfirm
 pacman -S vulkan-intel lib32-vulkan-intel --noconfirm
 pacman -S vulkan-radeon lib32-vulkan-radeon --noconfirm
